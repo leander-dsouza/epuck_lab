7146517500d8fa97f31a1d425136dc1a1f0def32
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2023-10-18 13:33:44 +0100

    Lab 0 done

--- Lab0/main.c
+++ Lab0/main.c
@@ -8,6 +8,11 @@
 #include "memory_protection.h"
 #include <main.h>
 
+#include "leds.h"
+#include "spi_comm.h"
+#include "motors.h"
+#include "selector.h"
+
 
 int main(void)
 {
@@ -15,12 +20,56 @@ int main(void)
     halInit();
     chSysInit();
     mpu_init();
+    clear_leds();
+    spi_comm_start();
+    motors_init();
+
+    unsigned int s;
 
 
     /* Infinite loop. */
     while (1) {
-    	//waits 1 second
-        chThdSleepMilliseconds(1000);
+//    	//waits 1 second
+//    	// ON
+//    	set_led(LED1, 1);
+//        chThdSleepMilliseconds(1000);
+//        // OFF
+//    	set_led(LED1, 0);
+//        chThdSleepMilliseconds(1000);
+//    	left_motor_set_speed(0);
+//    	right_motor_set_speed(0);
+    	s = get_selector();
+
+    	if (s == 0) {
+    		set_led(LED1, 1);
+    		set_led(LED2, 0);
+    		set_led(LED3, 0);
+    		set_led(LED4, 0);
+    		set_led(LED5, 0);
+    		set_led(LED6, 0);
+    		set_led(LED7, 0);
+    		set_led(LED8, 0);
+    	} else if (s == 1){
+    		set_led(LED1, 0);
+    		set_led(LED2, 1);
+    		set_led(LED3, 0);
+    		set_led(LED4, 0);
+    		set_led(LED5, 0);
+    		set_led(LED6, 0);
+    		set_led(LED7, 0);
+    		set_led(LED8, 0);
+    	} else {
+    		set_led(LED1, 0);
+    		set_led(LED2, 0);
+    		set_led(LED3, 0);
+    		set_led(LED4, 0);
+    		set_led(LED5, 0);
+    		set_led(LED6, 0);
+    		set_led(LED7, 0);
+    		set_led(LED8, 0);
+    	}
+
+
     }
 }
 
