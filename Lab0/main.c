beac4ec0d659f2ca5da9b87137dd0cbd165bae21
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2023-10-24 10:37:45 +0100

    task 1 precursors complete

--- Lab0/main.c
+++ Lab0/main.c
@@ -13,65 +13,122 @@
 #include "motors.h"
 #include "selector.h"
 
+#include "epuck1x/uart/e_uart_char.h"
+#include "stdio.h"
+#include "serial_comm.h"
+#include "sensors/proximity.h"
+
+
+const unsigned int MAX_SPEED = 1000;
+const unsigned int ESTOP_SELECTOR = 0;
+const unsigned int THRESHOLD = 50;
+
+// proximity sensors initialization
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
+
+
+void init() {
+  halInit();
+  chSysInit();
+  mpu_init();
+
+  // LED Initialization
+  clear_leds();
+  spi_comm_start();
+
+  // Motor Initialization
+  motors_init();
+
+  // Selector Initialization
+  serial_start();
+
+  // Proximity Initialization
+  messagebus_init(&bus, &bus_lock, &bus_condvar);
+  proximity_start(0);
+  calibrate_ir();
+}
+
+void move_forward(float speed) {
+  left_motor_set_speed(speed);
+  right_motor_set_speed(speed);
+}
+
+void move_backward(float speed) {
+  left_motor_set_speed(-speed);
+  right_motor_set_speed(-speed);
+}
+
+void turn_left(float speed) {
+  left_motor_set_speed(-speed);
+  right_motor_set_speed(speed);
+}
+
+void turn_right(float speed) {
+  left_motor_set_speed(speed);
+  right_motor_set_speed(-speed);
+}
+
+void stop() {
+  left_motor_set_speed(0);
+  right_motor_set_speed(0);
+}
+
 
 int main(void)
 {
+  init();
 
-    halInit();
-    chSysInit();
-    mpu_init();
-    clear_leds();
-    spi_comm_start();
-    motors_init();
+  /* Infinite loop. */
+  while (1) {
 
-    unsigned int s;
-
-
-    /* Infinite loop. */
-    while (1) {
-//    	//waits 1 second
-//    	// ON
-//    	set_led(LED1, 1);
-//        chThdSleepMilliseconds(1000);
-//        // OFF
-//    	set_led(LED1, 0);
-//        chThdSleepMilliseconds(1000);
-//    	left_motor_set_speed(0);
-//    	right_motor_set_speed(0);
-    	s = get_selector();
-
-    	if (s == 0) {
-    		set_led(LED1, 1);
-    		set_led(LED2, 0);
-    		set_led(LED3, 0);
-    		set_led(LED4, 0);
-    		set_led(LED5, 0);
-    		set_led(LED6, 0);
-    		set_led(LED7, 0);
-    		set_led(LED8, 0);
-    	} else if (s == 1){
-    		set_led(LED1, 0);
-    		set_led(LED2, 1);
-    		set_led(LED3, 0);
-    		set_led(LED4, 0);
-    		set_led(LED5, 0);
-    		set_led(LED6, 0);
-    		set_led(LED7, 0);
-    		set_led(LED8, 0);
-    	} else {
-    		set_led(LED1, 0);
-    		set_led(LED2, 0);
-    		set_led(LED3, 0);
-    		set_led(LED4, 0);
-    		set_led(LED5, 0);
-    		set_led(LED6, 0);
-    		set_led(LED7, 0);
-    		set_led(LED8, 0);
-    	}
-
-
+    if (get_selector() == ESTOP_SELECTOR) {
+      stop();
     }
+    else {
+      // send calibrated proximity values through bluetooth
+      // char prox_str[100];
+      // int str_length;
+      // str_length = sprintf(prox_str, "%d,%d,%d,%d,%d,%d,%d,%d\n",
+      //   get_calibrated_prox(0),
+      //   get_calibrated_prox(1),
+      //   get_calibrated_prox(2),
+      //   get_calibrated_prox(3),
+      //   get_calibrated_prox(4),
+      //   get_calibrated_prox(5),
+      //   get_calibrated_prox(6),
+      //   get_calibrated_prox(7)
+      // );
+      // e_send_uart1_char(prox_str, str_length);    // obstacle avoidance
+
+
+      // if object is on left, turn right
+      if (get_calibrated_prox(5) > THRESHOLD && get_calibrated_prox(6) > THRESHOLD && get_calibrated_prox(7) > THRESHOLD) {
+        turn_right(MAX_SPEED);
+      } else if (get_calibrated_prox(0) > THRESHOLD && get_calibrated_prox(1) > THRESHOLD && get_calibrated_prox(2) > THRESHOLD) {
+        turn_left(MAX_SPEED);
+      } else if (get_calibrated_prox(0) > THRESHOLD && get_calibrated_prox(1) > THRESHOLD) {
+        turn_left(MAX_SPEED);
+      } else if (get_calibrated_prox(5) > THRESHOLD && get_calibrated_prox(6) > THRESHOLD) {
+        turn_right(MAX_SPEED);
+      } else if (get_calibrated_prox(0) > THRESHOLD) {
+        turn_left(MAX_SPEED);
+      } else if (get_calibrated_prox(5) > THRESHOLD) {
+        turn_right(MAX_SPEED);
+      } else {
+        move_forward(MAX_SPEED);
+      }
+    }
+
+
+
+
+
+
+  }
 }
+
 
 #define STACK_CHK_GUARD 0xe2dee396
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
